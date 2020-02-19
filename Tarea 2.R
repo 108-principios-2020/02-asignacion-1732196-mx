61635bc3a76fd864592c9c097b435149b79e56b9
Uriel Sarmiento <uriel.sv@hotmail.com>
2020-02-19 15:11:31 -0600

    Actualizar Doc. ocupo ayuda para instalar el RStudio (´°¬°)

--- Tarea 2.R
+++ Tarea 2.R
@@ -26,8 +26,46 @@ esp
 barplot(esp$freq, names.arg = esp$x, main = "especies", col = "blue")
 
 
+# ¿Que especie presenta mayor proporción (%)?   ---------------------------
+
+# La especie que presenta mayor proporción es la Tsuga heterófila  --------
+
+# Con un porcentaje de 37.5% ----------------------------------------------
+
+
+
+
 # Ejercicio #3 ------------------------------------------------------------
 
 library(repmis)
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
+
+vecyesp <- table(conjunto$vecinos,conjunto$especies)
+vecyesp  
+
+# Ejercicio #4 ------------------------------------------------------------
+
+Altura <- conjunto$Altura
+range(Altura)
+
+Intervalo <- seq(8.47, 21.46, by=5)
+Intervalo
+
+Altura.table <- cut(Altura, Intervalo)
+table(Altura.table)
+
+Altura.prop <- cbind(table(Altura.table))
+Altura.per <- round(prop.table(Altura.prop)*100,2)
 
 
+# Ejercicio 5 -------------------------------------------------------------
+
+diametro <- conjunto$Diametro
+range(diametro)
+
+Intervalo <- seq(7.5, 25.5, by= 2.5)
+
+par(mfrow=c(1,2))
+hist(conjunto$Diametro, main = "Sin modificar", xlab = "Díametro", col = "pink")
+hist(conjunto$Diametro, breaks = Intervalo, main = "Datos Intervalos", col = "yellow")
+par(mfrow=c(1,1))
